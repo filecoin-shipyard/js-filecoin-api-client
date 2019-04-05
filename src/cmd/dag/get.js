c6e7b705c91f3c295299da3e92824331bee6cce5
Alan Shaw <alan.shaw@protocol.ai>
2019-04-05 15:55:15 +0100

    docs: add API docs for dag.get

--- src/cmd/dag/get.js
+++ src/cmd/dag/get.js
@@ -8,8 +8,11 @@ module.exports = (fetch, config) => {
     const url = `${toUri(config.apiAddr)}/api/dag/get?arg=${encodeURIComponent(cid)}`
     const res = await ok(fetch(url, { signal: options.signal }))
 
-    return res.headers.get('Content-Type') === 'application/json'
-      ? res.json()
-      : res.blob()
+    if (res.headers.get('Content-Type') === 'application/json') {
+      return res.json()
+    }
+
+    // Buffer for Node.js, blob otherwise
+    return res.buffer ? res.buffer() : res.blob()
   }
 }
